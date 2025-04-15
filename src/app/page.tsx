e25e078cda06ffedcf2045a4ce3c0f6548386d5a
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-04-15 04:02:07 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import Hero from "@/components/Hero";
 
 import TextOnScroll from "@/components/TextOnScroll";
 import ScrollSection from "@/components/SmoothScroll";
 import MyProjects from "@/components/MyProjects";
 import Contact from "@/components/Contact";
+import { useState } from "react";
+import PreLoadingShow from "@/components/PreLoadingShow";
+import { AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+    document.body.style.cursor = "default";
+    window.scrollTo(0, 0);
+  }, 2000);
   return (
     <ScrollSection>
+      <AnimatePresence mode="wait">
+        {loading && <PreLoadingShow />}
+      </AnimatePresence>
       <Hero />
 
       <TextOnScroll />
---
+++ src/components/PreLoadingShow.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+const PreLoadingShow = () => {
+  const [index, setIndex] = useState(0);
+  const texts = ["Hello everybody", "welcome to my page"];
+
+  useEffect(() => {
+    if (index == texts.length - 1) return;
+
+    setTimeout(
+      () => {
+        setIndex(index + 1);
+      },
+      index == 0 ? 1000 : 150
+    );
+  }, [index]);
+
+  const opacity = {
+    initial: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 0.75,
+      transition: { duration: 1, delay: 0.2 },
+    },
+  };
+
+  const slideUpAnimation = {
+    initial: {
+      top: 0,
+    },
+    exit: {
+      top: "-100vh",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 },
+    },
+  };
+  return (
+    <motion.div
+      variants={slideUpAnimation}
+      initial="initial"
+      exit="exit"
+      className="h-screen w-screen bg-[#161310] fixed top-0 left-0 z-[999] text-[#ecebeb] flex justify-center items-center"
+    >
+      <motion.p
+        className="font-serif font-semibold text-3xl sm:text-5xl"
+        variants={opacity}
+        initial="initial"
+        animate="enter"
+      >
+        {texts[index]}
+      </motion.p>
+    </motion.div>
+  );
+};
+
+export default PreLoadingShow;
