c62523b7333aef510049ea34057334c138a0d4dd
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-05-09 01:22:03 +0100

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     >
       <div className="relative  h-[calc(100vh+420px)] -top-[100vh] bg-[#161310]  flex-col justify-start">
         <div className="h-[420px] text-[#ecebeb] p-14 sticky top-[calc(100vh-420px)]">
-          <div className="w-full h-[270px] text-2xl px-5  sm:text-6xl md:text-8xl flex flex-col justify-center items-center  font-semibold text-nowrap">
+          <div className="w-full h-[270px] text-2xl px-5  sm:text-6xl  flex flex-col justify-center items-center  font-semibold text-nowrap">
             <h1 className="whitespace-normal opacity-75">Jonas Nygaard,</h1>
             <h1 className="whitespace-nowrap opacity-75">
               Designer & Developer
