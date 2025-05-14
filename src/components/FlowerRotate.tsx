03b8b960047620ed0bf53541a769995624996410
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-05-15 00:11:12 +0100

    .

--- src/components/FlowerRotate.tsx
+++ src/components/FlowerRotate.tsx
@@ -1,24 +1,9 @@
 "use client";
-import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function FlowerRotate() {
-  const ref = useRef(null);
-
-  // 1) start measuring *way* below the viewport
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "end start"],
-  });
-
-  // 2) map only the first 5% of that long scroll to the draw
-  const pathLength = useTransform(scrollYProgress, [0, 0.05], [0, 1]);
-
   return (
-    <div
-      ref={ref}
-      className="absolute top-[55%] left-1/2 transform -translate-x-1/2 w-[70px] h-[70px] pointer-events-none z-10"
-    >
+    <div className="absolute top-[55%] left-1/2 transform -translate-x-1/2 w-[70px] h-[70px] pointer-events-none z-10">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 89.294 86.555"
@@ -31,15 +16,18 @@ export default function FlowerRotate() {
             fill="none"
             stroke="#1c1a17"
             strokeWidth="2"
-            style={{ pathLength }}
-            transition={{ duration: 0.2 }}
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.8 }}
           />
           <motion.path
             d="M53.408,286.664c-.244-82.658,2.3-83.751,2.3-83.751l29.859,83.751V202.913"
             fill="none"
             stroke="#1c1a17"
             strokeWidth="2"
-            style={{ pathLength }}
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
           />
         </g>
       </svg>
