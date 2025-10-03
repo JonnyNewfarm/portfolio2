5abf02cb5f8bc6ced7ca398ff637b4855207e2cb
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-10-03 18:17:21 +0100

    :

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,12 @@
 import Hero from "@/components/Hero";
 
 import ScrollSection from "@/components/SmoothScroll";
-import ThemeWrapper from "@/components/ThemeWrapper";
 
 export default function Home() {
   return (
     <ScrollSection>
       <div className=" relative">
-        <ThemeWrapper>
-          <Hero />
-        </ThemeWrapper>
+        <Hero />
       </div>
     </ScrollSection>
   );
