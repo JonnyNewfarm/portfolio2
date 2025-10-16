25af41ab65dd42ff2e5eacc2ccd8f5ecaceca1b9
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-10-16 20:48:11 +0100

    :

--- src/components/hero/CartoonModel.tsx
+++ src/components/hero/CartoonModel.tsx
@@ -19,8 +19,6 @@ export default function CartoonModel({
     leftLeg?: THREE.Object3D;
     rightLeg?: THREE.Object3D;
     spine?: THREE.Object3D;
-    spine1?: THREE.Object3D;
-    spine2?: THREE.Object3D;
     rightArm?: THREE.Object3D;
   }>({});
 
@@ -31,21 +29,17 @@ export default function CartoonModel({
     bones.current.rightLeg = group.current.getObjectByName("RightLeg");
     bones.current.spine = group.current.getObjectByName("Spine");
     bones.current.rightArm = group.current.getObjectByName("RightArm");
-    bones.current.spine2 = group.current.getObjectByName("Spine2");
-    bones.current.spine1 = group.current.getObjectByName("Spine1");
 
     if (bones.current.hip) {
       bones.current.hip.rotation.x = -Math.PI / 2.7;
       // Move hips slightly backward toward the seatback
-      bones.current.hip.position.z -= 0.015; // try between 0.03–0.1 for fine-tuning
+      bones.current.hip.position.z -= 0; // try between 0.03–0.1 for fine-tuning
     }
 
     if (bones.current.leftLeg) bones.current.leftLeg.rotation.x = -Math.PI / 2;
     if (bones.current.rightLeg)
       bones.current.rightLeg.rotation.x = -Math.PI / 2.5;
-    if (bones.current.spine) bones.current.spine.rotation.x = Math.PI / 1.9;
-    if (bones.current.spine2) bones.current.spine2.rotation.x = -0.2;
-    if (bones.current.spine1) bones.current.spine1.rotation.x = -0.4;
+    if (bones.current.spine) bones.current.spine.rotation.x = Math.PI / 2.8;
 
     if (bones.current.rightArm) {
       bones.current.rightArm.rotation.x = Math.PI / 2.3;
@@ -58,8 +52,8 @@ export default function CartoonModel({
 
     const t = state.clock.getElapsedTime();
 
-    group.current.position.y = -0.45 + Math.sin(t * 1.2) * 0.008;
-    group.current.rotation.y = Math.PI + Math.sin(t * 0.3) * 0.009;
+    group.current.position.y = -0.45;
+    group.current.rotation.y = Math.PI + Math.sin(t * 0.3) * 0.002;
 
     // Scroll-based arm lift
     const progress = scrollYProgress.get();
@@ -69,7 +63,7 @@ export default function CartoonModel({
   });
 
   return (
-    <mesh position={[0.14, 0.011, 2.86]}>
+    <mesh position={[0.14, 0.011, 2.81]}>
       <primitive ref={group} object={scene} scale={1.46} />
     </mesh>
   );
